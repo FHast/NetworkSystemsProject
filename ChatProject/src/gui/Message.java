8bd92fdb929361a852994a38bbe3c15884b80ac5
gereon <gereon@gereonLaptop>
2017-04-10 14:39:17 +0200

    message timestamps

--- ChatProject/src/gui/Message.java
+++ ChatProject/src/gui/Message.java
@@ -1,24 +1,33 @@
 package gui;
 
+import java.time.LocalTime;
+
 public class Message {
-	
+
 	private boolean isSentBySelf;
 	private String text;
-	//private long timestamp;
-	
-	public Message(boolean sentBySelf, String text/*, long timestamp*/) {
+	private LocalTime timestamp;
+
+	public Message(boolean sentBySelf, String text/* , long timestamp */) {
 		this.isSentBySelf = sentBySelf;
 		this.text = text;
-		//this.timestamp = timestamp;
+		timestamp = LocalTime.now();
 	}
-	
+
 	public boolean isSentBySelf() {
 		return isSentBySelf;
 	}
-	
+
 	public String getText() {
 		return text;
 	}
-	
-	
+
+	public String toString() {
+		if (!isSentBySelf) {
+			return "[" + timestamp + "] " + text;
+		} else {
+			return text + " [" + timestamp + "]";
+		}
+	}
+
 }
