c8fb7764500b0bc46e551f613a539e9d470af728
fabian <fabian@fabian-pc>
2017-04-11 09:41:52 +0200

    added error msg to NewContactPopup

--- ChatProject/src/gui/NewContactPopup.java
+++ ChatProject/src/gui/NewContactPopup.java
@@ -17,6 +17,7 @@ public class NewContactPopup extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtDevice;
 	private JTextField txtName;
+	private JLabel lblErrorMsg;
 
 	/**
 	 * Create the frame.
@@ -25,7 +26,7 @@ public class NewContactPopup extends JFrame {
 		setTitle("New contact");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 307, 226);
+		setBounds(100, 100, 330, 226);
 		contentPane = new JPanel();
 
 		JLabel lblTop = new JLabel("Please enter the contact details!");
@@ -55,32 +56,55 @@ public class NewContactPopup extends JFrame {
 					} catch (NumberFormatException ex) {
 						txtName.setText("");
 						txtDevice.setText("");
+						lblErrorMsg.setText("Please enter a number!");
 					}
+				}
+				else {
+					lblErrorMsg.setText("You have to enter something..");
 				}
 			}
 
 		});
+		
+		lblErrorMsg = new JLabel("");
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addContainerGap()
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-										.addComponent(lblTop).addComponent(lblDevice).addComponent(lblName)
-										.addComponent(txtDevice, GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
-										.addComponent(txtName))
-								.addComponent(btnFinish))
-						.addContainerGap(154, Short.MAX_VALUE)));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
-				.createSequentialGroup().addContainerGap().addComponent(lblTop)
-				.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(lblDevice)
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addComponent(txtDevice, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-						GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblName)
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addGap(18).addComponent(btnFinish).addContainerGap(99, Short.MAX_VALUE)));
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+							.addComponent(lblTop)
+							.addComponent(lblDevice)
+							.addComponent(lblName)
+							.addComponent(txtDevice, GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)
+							.addComponent(txtName))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnFinish)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(lblErrorMsg)))
+					.addContainerGap(19, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblTop)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblDevice)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtDevice, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblName)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnFinish)
+						.addComponent(lblErrorMsg))
+					.addContainerGap(25, Short.MAX_VALUE))
+		);
 		contentPane.setLayout(gl_contentPane);
 		setContentPane(contentPane);
 	}
