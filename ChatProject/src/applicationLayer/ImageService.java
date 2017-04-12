1d09c8a8e8e0adac5f4aba5e8bd33d0722a66040
gereon <gereon@gereonLaptop>
2017-04-12 13:00:17 +0200

    working file converter

--- ChatProject/src/applicationLayer/ImageService.java
+++ ChatProject/src/applicationLayer/ImageService.java
@@ -3,6 +3,7 @@ package applicationLayer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.Random;
 
 import org.apache.commons.io.FileUtils;
@@ -11,30 +12,39 @@ public class ImageService {
 	public static String fileToString(String url) {
 		try {
 			File file = new File(url);
-			String data = FileUtils.readFileToString(file, "Base64");
-			return data;
+			byte[] bytes = FileUtils.readFileToByteArray(file);
+			byte[] encoded = Base64.getEncoder().encode(bytes);
+			return new String(encoded);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "";
+		return null;
 	}
 
 	public static void stringToFile(String data, String filetype) {
 		try {
 			String s = "" + new Random().nextInt();
 			File file = new File(s + "." + filetype);
-			FileUtils.writeStringToFile(file, data, "Base64");
+			byte[] encoded = data.getBytes();
+			byte[] decoded = Base64.getDecoder().decode(encoded);
+			FileUtils.writeByteArrayToFile(file, decoded);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
-		String s = fileToString("chaticon.png");
-		System.out.println(s);
-		stringToFile(s, "txt");
+	public static String getAppendix(String url) {
+		return url.split("[.]")[1];
+	}
+
+	public static String getName(String url) {
+		return url.split("[.]")[0];
+	}
+
+	public static String fileToString(String name, String appendix) {
+		return fileToString(name + "." + appendix);
 	}
 }
